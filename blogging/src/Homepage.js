262bfe0f3a6121c813f379852239622bab0ae0d2
Dipesh Gund <dipeshgund229@gmail.com>
2021-02-01 14:06:22 +0530

    Added Homepage.js and navbar.js

---
+++ blogging/src/Homepage.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="Home">
+            <h1>Welcome to blogger's hub</h1>
+        </div>
+    );
+}
+    <div className="Home">
+        <h1>Welcome to blogger's hub</h1>
+    </div>
+export default Home;
---
+++ blogging/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (  
+        <div className="Navbar">
+            <nav className="navbar">
+                <h1>bLoGgEr'S hUb</h1>
+                <div className="links">
+                    <a href="/">Home</a><br/>
+                    <a href="/create">Create</a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
